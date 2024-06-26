d54ea20bf201b51573d3fe4bc0593632fc1aefa7
Lxcier <dev.lxcier@gmail.com>
2024-06-25 22:27:06 -0300

    fix: animações

--- src/scrollReveal.js
+++ src/scrollReveal.js
@@ -6,5 +6,37 @@ const sr = ScrollReveal({
   distance: '40px',
 });
 
+sr.reveal('.animated');
+
+sr.reveal('.section__title', {
+  duration: 600,
+  scale: .8,
+  origin: 'bottom',
+});
+
+sr.reveal('.paragraph', {
+  delay: 200,
+});
+
+sr.reveal ('.animated-right', {
+  origin: 'right'
+});
+
+sr.reveal('.animated-left', {
+  origin: 'left'
+});
+
+
+// cards
+sr.reveal('.card', {
+  interval: 200,
+  origin: 'left',
+});
+
+sr.reveal('.arrow', {
+  interval: 200,
+  origin: 'left',
+});
+
 
 export default sr; 
